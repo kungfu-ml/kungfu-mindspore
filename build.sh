6a493a5d4d4d9214640ba835204130e9b9429a49
lg <lg4869@outlook.com>
2021-01-02 16:47:16 +0000

    export ENABLE_KUNGFU=ON

--- build.sh
+++ build.sh
@@ -11,10 +11,11 @@ git checkout -f
 cp -r ../ops mindspore/
 cp -r ../ccsrc mindspore/
 git apply ../patches/$TAG/a.patch
-git diff
-git status
+# git diff
+# git status
 
 CUDA_HOME=/usr/local/cuda
 export CUDACXX=$CUDA_HOME/bin/nvcc
+export ENABLE_KUNGFU=ON
 
 ./build.sh -e gpu
