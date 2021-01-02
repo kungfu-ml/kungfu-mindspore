63dd3fd508fafb2061d3df6411c716b59c8e4a3c
lg <lg4869@outlook.com>
2021-01-02 16:12:54 +0000

    build

--- build.sh
+++ build.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 set -e
 
-cp -r ops mindspore/mindspore/
-cp -r ccsrc mindspore/mindspore/
+cd $(dirname $0)
+
+TAG=$(cat tag.txt)
 
 cd mindspore
+git checkout -f
+
+cp -r ../ops mindspore/
+cp -r ../ccsrc mindspore/
+git apply ../patches/$TAG/a.patch
+git diff
+git status
 
 CUDA_HOME=/usr/local/cuda
 export CUDACXX=$CUDA_HOME/bin/nvcc
