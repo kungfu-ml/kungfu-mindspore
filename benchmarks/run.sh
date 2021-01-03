dc00801422461c10c3fd797be867386ec0c4cded
lg <lg4869@outlook.com>
2021-01-03 22:11:27 +0000

    import benchmark

---
+++ benchmarks/run.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+set -e
+
+cd $(dirname $0)
+ROOT=$PWD/../mindspore
+
+KUNGFU_LIB_PATH=$ROOT/third_party/kungfu/lib
+# CUDA_HOME=/usr/local/cuda
+
+# export LD_LIBRARY_PATH=$KUNGFU_LIB_PATH
+export LD_LIBRARY_PATH=$KUNGFU_LIB_PATH:$ROOT/mindspore/lib:$ROOT/build/mindspore/_deps/ompi-src/ompi/.libs
+# export LD_LIBRARY_PATH=$PWD/mindspore/lib:$ROOT/build/mindspore/_deps/ompi-src/ompi/.libs
+
+# export PATH=$PATH:$CUDA_HOME/bin
+
+# export KUNGFU_MINDSPORE_DEBUG=1
+
+kungfu_run_flags() {
+    echo -q
+    echo -logdir logs
+    echo -logfile kungfu-run.log
+    echo -np $np
+}
+
+kungfu_run() {
+    kungfu-run $(kungfu_run_flags) $@
+}
+
+app_flags() {
+    # echo --device CPU
+    echo --device GPU
+
+    echo --warmup-steps 1
+    echo --steps 8
+
+    # echo --model empty
+    # echo --model vgg16
+    echo --model resnet50
+    # echo --collective mindspore
+    # echo --collective kungfu
+}
+
+trace() {
+    echo "BEGIN $@"
+    $@
+    echo "END $@"
+    echo
+    echo
+}
+
+main() {
+    # kungfu_run python3.7 ./hello_world.py $(app_flags)
+    # for np in $(seq  4); do
+    np=4
+    trace kungfu_run python3.7 ./benchmark_all_reduce.py $(app_flags)
+    # mpi_run python3.7 ./benchmark_all_reduce.py $(app_flags)
+}
+
+rm -fr logs
+# export GLOG_v=0
+main
