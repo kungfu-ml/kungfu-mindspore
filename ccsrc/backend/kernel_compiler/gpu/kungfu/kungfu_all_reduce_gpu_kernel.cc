5bd2e1d460428428fdb4aab1dd14ebee74591157
lg <lg4869@outlook.com>
2021-01-03 23:56:40 +0000

    fix LD_LIBRARY_PATH

--- ccsrc/backend/kernel_compiler/gpu/kungfu/kungfu_all_reduce_gpu_kernel.cc
+++ ccsrc/backend/kernel_compiler/gpu/kungfu/kungfu_all_reduce_gpu_kernel.cc
@@ -5,5 +5,7 @@ namespace mindspore {
 namespace kernel {
 MS_REG_GPU_KERNEL_ONE(KungFuAllReduce, KernelAttr().AddInputAttr(kNumberTypeFloat32).AddOutputAttr(kNumberTypeFloat32),
                       KungFuAllReduceGpuKernel, float)
+MS_REG_GPU_KERNEL_ONE(KungFuAllReduce, KernelAttr().AddInputAttr(kNumberTypeInt32).AddOutputAttr(kNumberTypeInt32),
+                      KungFuAllReduceGpuKernel, int32_t)
 }  // namespace kernel
 }  // namespace mindspore
--- tests/run.sh
+++ tests/run.sh
@@ -1,12 +1,24 @@
 #!/bin/sh
 set -e
 
+join_path() {
+    local IFS=":"
+    echo "$*"
+}
+
 cd $(dirname $0)
 ROOT=$PWD/../mindspore
 
 KUNGFU_LIB_PATH=$ROOT/third_party/kungfu/lib
 
-export LD_LIBRARY_PATH=$KUNGFU_LIB_PATH:$ROOT/mindspore/lib:$ROOT/build/mindspore/_deps/ompi-src/ompi/.libs:$ROOT/build/mindspore/_deps/nccl-src/build/lib
+ld_library_path() {
+    echo $KUNGFU_LIB_PATH
+    echo $ROOT/mindspore/lib
+    echo $ROOT/build/mindspore/_deps/ompi-src/ompi/.libs
+    # echo $ROOT/build/mindspore/_deps/nccl-src/build/lib
+}
+
+export LD_LIBRARY_PATH=$(join_path $(ld_library_path))
 
 kungfu_run_flags() {
     echo -q
@@ -16,6 +28,7 @@ kungfu_run_flags() {
 
 kungfu_run() {
     kungfu-run $(kungfu_run_flags) $@
+    echo
 }
 
 test_broadcast_op() {
